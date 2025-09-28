1fd5c8a2ddf87bbfa3b441351729aa2810241d60
kikoff1 <neris54@gmail.com>
2025-09-28 19:57:07 +0300

    Update person_tst.js

--- person_tst.js
+++ person_tst.js
@@ -1,53 +1,12 @@
 (function() {
     "use strict";
 
-    // ==== ПРИХОВАННЯ СТАНДАРТНОЇ КНОПКИ "ПІДПИСАТИСЯ" ====
-    function hideSubscribeButton() {
-        if (document.getElementById('hide-subscribe-style')) return;
-
-        const css = `
-            .button--subscribe {
-                display: none !important;
-            }
-        `;
-
-        const style = document.createElement('style');
-        style.id = 'hide-subscribe-style';
-        style.textContent = css;
-        document.head.appendChild(style);
-    }
-
     // ==== ОСНОВНІ ЗМІННІ ====
     var PLUGIN_NAME = "persons_plugin";
     var PERSONS_KEY = "saved_persons";
     var PAGE_SIZE = 20;
     var DEFAULT_PERSON_IDS = [];
     var currentPersonId = null;
-
-    var pluginTranslations = {
-        persons_title: {
-            ru: "Персоны",
-            en: "Persons",
-            uk: "Персони"
-        },
-        subscriibbe: {
-            en: "subscriibbe",
-            uk: "Підписатися",
-            ru: "Подписаться"
-        },
-        unsubscriibbe: {
-            en: "unsubscriibbe",
-            uk: "Відписатися",
-            ru: "Отписаться"
-        },
-        persons_not_found: {
-            en: "No persons found",
-            uk: "Особи не знайдені",
-            ru: "Персоны не найдены"
-        }
-    };
-
-    var ICON_SVG = '<svg height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 11C17.66 11 18.99 9.66 18.99 8C18.99 6.34 17.66 5 16 5C14.34 5 13 6.34 13 8C13 9.66 14.34 11 16 11ZM8 11C9.66 11 10.99 9.66 10.99 8C10.99 6.34 9.66 5 8 5C6.34 5 5 6.34 5 8C5 9.66 6.34 11 8 11ZM8 13C5.67 13 1 14.17 1 16.5V19H15V16.5C15 14.17 10.33 13 8 13ZM16 13C15.71 13 15.38 13.02 15.03 13.05C16.19 13.89 17 15.02 17 16.5V19H23V16.5C23 14.17 18.33 13 16 13Z" fill="currentColor"/></svg>';
 
     // ==== ЗБЕРІГАННЯ ====
     function initStorage() {
@@ -191,19 +150,9 @@
 
     // ==== СТАРТ ПЛАГІНА ====
     function startPlugin() {
-        hideSubscribeButton();
-
-        Lampa.Lang.add({
-            persons_plugin_title: pluginTranslations.persons_title,
-            persons_plugin_subscriibbe: pluginTranslations.subscriibbe,
-            persons_plugin_unsubscriibbe: pluginTranslations.unsubscriibbe,
-            persons_plugin_not_found: pluginTranslations.persons_not_found,
-        });
-
         initStorage();
 
-        var personsService = new PersonsService();
-        Lampa.Api.sources[PLUGIN_NAME] = personsService;
+        Lampa.Api.sources[PLUGIN_NAME] = new PersonsService();
 
         // меню "Персони"
         var menuItem = $(
