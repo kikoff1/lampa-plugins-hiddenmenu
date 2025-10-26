da37922ad18a9f2c9bbe1b9fb0f6170313363381
kikoff1 <neris54@gmail.com>
2025-10-26 10:06:54 +0200

    Update actors_v3.js

--- actors_v3.js
+++ actors_v3.js
@@ -52,22 +52,30 @@
             patchActorsPage: function() {  
                 var self = this;  
                   
-                // Перехоплюємо створення активності  
                 Lampa.Listener.follow('activity', function(e) {  
                     if (e.type === 'create' && e.component === 'category_full') {  
                         var activity = Lampa.Activity.active();  
                           
                         if (activity.url === 'person/popular') {  
-                            // Чекаємо, поки картки створяться  
                             setTimeout(function() {  
-                                // Знаходимо всі картки на сторінці  
-                                var cards = document.querySelectorAll('.category-full .card');  
+                                var cards = document.querySelectorAll('.card');  
                                   
                                 cards.forEach(function(cardElement) {  
-                                    // Отримуємо дані картки  
-                                    var card_data = cardElement.card_data;  
+                                    var cardData = null;  
                                       
-                                    if (card_data && card_data.gender !== undefined && card_data.profile_path) {  
+                                    // Спробуємо знайти дані картки  
+                                    if (activity.activity && activity.activity.component && activity.activity.component.items) {  
+                                        var items = activity.activity.component.items;  
+                                        for (var i = 0; i < items.length; i++) {  
+                                            if (items[i].render && items[i].render(true) === cardElement) {  
+                                                cardData = items[i].data;  
+                                                break;  
+                                            }  
+                                        }  
+                                    }  
+                                      
+                                    // Перевіряємо наявність profile_path замість gender  
+                                    if (cardData && cardData.profile_path) {  
                                         // Видаляємо старий обробник  
                                         var newCard = cardElement.cloneNode(true);  
                                         cardElement.parentNode.replaceChild(newCard, cardElement);  
@@ -75,11 +83,11 @@
                                         // Додаємо новий обробник, який відкриває компонент actor  
                                         newCard.addEventListener('hover:enter', function() {  
                                             Lampa.Activity.push({  
-                                                url: card_data.url || '',  
+                                                url: cardData.url || '',  
                                                 title: Lampa.Lang.translate('title_person'),  
                                                 component: 'actor',  
-                                                id: card_data.id,  
-                                                source: card_data.source || 'tmdb'  
+                                                id: cardData.id,  
+                                                source: 'tmdb'  
                                             });  
                                         });  
                                     }  
@@ -91,29 +99,31 @@
             },  
   
             toggleActorsButton: function() {  
-                $('.online-cinemas-actors').toggle(this.settings.showActors);  
+                var button = $('.online-cinemas-actors');  
+                if(this.settings.showActors) {  
+                    button.show();  
+                } else {  
+                    button.hide();  
+                }  
             },  
   
             addActorsButton: function() {  
-                const ico = '<svg xmlns="http://www.w3.org/2000/svg" width="2.2em" height="2.2em" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-width="4"><path stroke-linejoin="round" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/><path d="M30 24v-4.977C30 16.226 28.136 14 24 14s-6 2.226-6 5.023V24"/><path stroke-linejoin="round" d="M30 24h-6v-4.977C24 16.226 25.864 14 30 14s6 2.226 6 5.023V24h-6Zm-18 0h6v-4.977C24 16.226 22.136 14 18 14s-6 2.226-6 5.023V24h6Z"/></g></svg>';  
-                const button = $(`<li class="menu__item selector online-cinemas-actors" data-action="actors">  
-                    <div class="menu__ico">${ico}</div>  
-                    <div class="menu__text">Актори</div>  
-                </li>`);  
+                var self = this;  
+                var ico = '<svg xmlns="http://www.w3.org/2000/svg" width="2.2em" height="2.2em" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-width="4"><path stroke-linejoin="round" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/><path d="M30 24v-4.977C30 16.226 28.136 14 24 14s-6 2.226-6 5.023V24"/><path stroke-linejoin="round" d="M30 24h-6v-4.977C24 16.226 25.864 14 30 14s6 2.226 6 5.023V24h-6Zm-18 0h6v-4.977C24 16.226 22.136 14 18 14s-6 2.226-6 5.023V24h6Z"/></g></svg>';  
+                var button = $('<li class="menu__item selector online-cinemas-actors" data-action="actors"><div class="menu__ico">' + ico + '</div><div class="menu__text">Актори</div></li>');  
   
-                button.on('hover:enter', this.showActors.bind(this));  
+                button.on('hover:enter', function() {  
+                    Lampa.Activity.push({  
+                        url: 'person/popular',  
+                        title: 'Актори',  
+                        component: 'category_full',  
+                        source: 'tmdb',  
+                        page: 1  
+                    });  
+                });  
+  
                 $('.menu .menu__list').eq(0).append(button);  
                 this.toggleActorsButton();  
-            },  
-  
-            showActors: function() {  
-                Lampa.Activity.push({  
-                    url: 'person/popular',  
-                    title: 'Актори',  
-                    component: 'category_full',  
-                    source: 'tmdb',  
-                    page: 1  
-                });  
             }  
         };  
   
