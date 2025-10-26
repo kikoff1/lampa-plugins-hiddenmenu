5acc40edd96940af83d1900d2e62fe34bb146874
kikoff1 <neris54@gmail.com>
2025-10-26 22:59:38 +0200

    Update actors_v3.js

--- actors_v3.js
+++ actors_v3.js
@@ -1,11 +1,5 @@
 (function () {  
     'use strict';  
-
-
-
-
-
-
   
     function startPlugin() {  
         if (window.plugin_online_cinemas_ready) return;  
@@ -21,7 +15,6 @@
                 this.createSettings();  
                 this.addActorsButton();  
                 this.initStorageListener();  
-                this.patchCategoryComponent();  
             },  
   
             loadSettings: function() {  
@@ -31,8 +24,8 @@
             createSettings: function() {  
                 Lampa.SettingsApi.addComponent({  
                     component: 'online_cinemas',  
-                    name: 'Популярні актори',  
-                    icon: '<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="18" cy="12" r="6" stroke="white" stroke-width="2"/><path d="M6 30c0-6.627 5.373-12 12-12s12 5.373 12 12" stroke="white" stroke-width="2"/></svg>'  
+                    icon: '<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="18" cy="18" r="17" stroke="white" stroke-width="2"/><path d="M18 10C14.69 10 12 12.69 12 16C12 19.31 14.69 22 18 22C21.31 22 24 19.31 24 16C24 12.69 21.31 10 18 10ZM18 20C15.79 20 14 18.21 14 16C14 13.79 15.79 12 18 12C20.21 12 22 13.79 22 16C22 18.21 20.21 20 18 20ZM18 24C13.58 24 10 25.79 10 28V30H26V28C26 25.79 22.42 24 18 24Z" fill="white"/></svg>',  
+                    name: 'Популярні актори'  
                 });  
   
                 Lampa.SettingsApi.addParam({  
@@ -52,72 +45,31 @@
             initStorageListener: function() {  
                 Lampa.Storage.listener.follow('change', (e) => {  
                     if (e.name === 'show_actors') {  
+                        this.settings.showActors = e.value;  
                         this.toggleActorsButton();  
                     }  
                 });  
             },  
   
             toggleActorsButton: function() {  
-                const button = $('.menu .menu__item').filter(function() {  
-                    return $(this).find('.menu__text').text() === 'Актори';  
-                });  
-  
+                const button = $('.menu .menu__list li[data-action="actors"]');  
                 if (this.settings.showActors) {  
-                    button.removeClass('hide');  
+                    button.show();  
                 } else {  
-                    button.addClass('hide');  
+                    button.hide();  
                 }  
             },  
   
-            patchCategoryComponent: function() {  
-                // Патчимо створення активності для person/popular  
-                const originalActivityPush = Lampa.Activity.push;  
-                  
-                Lampa.Activity.push = function(params) {  
-                    const result = originalActivityPush.call(this, params);  
-                      
-                    // Якщо це сторінка person/popular  
-                    if (params.url === 'person/popular' && params.component === 'category_full') {  
-                        // Чекаємо створення компонента  
-                        setTimeout(() => {  
-                            const activity = Lampa.Activity.active();  
-                            if (activity && activity.component) {  
-                                const component = activity.component;  
-                                  
-                                // Додаємо cardRender callback  
-                                component.cardRender = function(object, element, card) {  
-                                    // Перевіряємо, чи це актор (має profile_path або gender)  
-                                    if (element.profile_path || typeof element.gender !== 'undefined') {  
-                                        // Перевизначаємо onEnter для відкриття компонента actor  
-                                        card.onEnter = function(target, card_data) {  
-                                            Lampa.Activity.push({  
-                                                url: '',  
-                                                title: card_data.name || Lampa.Lang.translate('title_person'),  
-                                                component: 'actor',  
-                                                id: card_data.id,  
-                                                source: 'tmdb'  
-                                            });  
-                                        };  
-                                    }  
-                                };  
-                            }  
-                        }, 100);  
-                    }  
-                      
-                    return result;  
-                };  
-            },  
-  
             addActorsButton: function() {  
-                const button = $(`<li class="menu__item selector">  
-                    <div class="menu__ico">  
-                        <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">  
-                            <circle cx="18" cy="12" r="6" stroke="white" stroke-width="2"/>  
-                            <path d="M6 30c0-6.627 5.373-12 12-12s12 5.373 12 12" stroke="white" stroke-width="2"/>  
-                        </svg>  
-                    </div>  
-                    <div class="menu__text">Актори</div>  
-                </li>`);  
+                const button = $(`<li class="menu__item selector" data-action="actors">  
+                <div class="menu__item-icon">  
+                    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">  
+                        <circle cx="18" cy="18" r="17" stroke="white" stroke-width="2"/>  
+                        <path d="M18 10C14.69 10 12 12.69 12 16C12 19.31 14.69 22 18 22C21.31 22 24 19.31 24 16C24 12.69 21.31 10 18 10ZM18 20C15.79 20 14 18.21 14 16C14 13.79 15.79 12 18 12C20.21 12 22 13.79 22 16C22 18.21 20.21 20 18 20ZM18 24C13.58 24 10 25.79 10 28V30H26V28C26 25.79 22.42 24 18 24Z" fill="white"/>  
+                    </svg>  
+                </div>  
+                <div class="menu__item-text">Актори</div>  
+            </li>`);  
   
                 button.on('hover:enter', this.showActors.bind(this));  
                 $('.menu .menu__list').eq(0).append(button);  
@@ -125,12 +77,44 @@
             },  
   
             showActors: function() {  
-                Lampa.Activity.push({  
-                    url: "person/popular",  
-                    title: "Популярні актори",  
-                    component: "category_full",  
-                    source: "tmdb",  
+                // Використовуємо прямий виклик API для завантаження акторів  
+                Lampa.Api.list({  
+                    url: 'person/popular',  
+                    source: 'tmdb',  
                     page: 1  
+                }, (data) => {  
+                    // Додаємо gender до всіх акторів, якщо його немає  
+                    if (data.results) {  
+                        data.results.forEach(actor => {  
+                            if (typeof actor.gender === 'undefined') {  
+                                actor.gender = 1; // Додаємо gender  
+                            }  
+                        });  
+                    }  
+  
+                    // Відкриваємо активність з модифікованими даними  
+                    Lampa.Activity.push({  
+                        url: 'person/popular',  
+                        title: 'Популярні актори',  
+                        component: 'category_full',  
+                        source: 'tmdb',  
+                        card_type: true,  
+                        page: 1,  
+                        // Додаємо card_events для перевизначення onEnter  
+                        card_events: {  
+                            onEnter: function(target, card_data) {  
+                                if (card_data.profile_path || card_data.gender) {  
+                                    Lampa.Activity.push({  
+                                        url: '',  
+                                        title: card_data.name,  
+                                        component: 'actor',  
+                                        id: card_data.id,  
+                                        source: 'tmdb'  
+                                    });  
+                                }  
+                            }  
+                        }  
+                    });  
                 });  
             }  
         };  
