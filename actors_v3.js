8d4f13caf0c0685c7c62333b772148dd966550f0
kikoff1 <neris54@gmail.com>
2025-10-26 12:03:24 +0200

    Update actors_v3.js

--- actors_v3.js
+++ actors_v3.js
@@ -1,16 +1,6 @@
 (function () {  
     'use strict';  
   
-
-
-
-
-
-
-
-
-
-
     function startPlugin() {  
         if (window.plugin_online_cinemas_ready) return;  
         window.plugin_online_cinemas_ready = true;  
@@ -24,7 +14,6 @@
                 this.loadSettings();  
                 this.createSettings();  
                 this.initStorageListener();  
-                this.patchActorsPage();  
             },  
   
             loadSettings: function() {  
@@ -46,7 +35,8 @@
                     },  
                     field: {  
                         name: 'Показувати кнопку акторів'  
-                    }  
+                    },  
+                    onChange: this.toggleActorsButton.bind(this)  
                 });  
             },  
   
@@ -59,48 +49,17 @@
                 });  
             },  
   
-            patchActorsPage: function() {  
-                var self = this;  
-                  
-                Lampa.Listener.follow('activity', function(e) {  
-                    if (e.type === 'create' && e.object.component === 'category_full' && e.object.url === 'person/popular') {  
-                        setTimeout(function() {  
-                            var activity = Lampa.Activity.active();  
-                            if (!activity || !activity.activity || !activity.activity.component) return;  
-                              
-                            var cards = $('.card.selector');  
-                              
-                            cards.each(function() {  
-                                var card = $(this);  
-                                var cardData = card.data('card_data');  
-                                  
-                                if (cardData && cardData.profile_path) {  
-                                    card.off('hover:enter').on('hover:enter', function() {  
-                                        Lampa.Activity.push({  
-                                            url: '',  
-                                            title: Lampa.Lang.translate('title_person'),  
-                                            component: 'actor',  
-                                            id: cardData.id,  
-                                            source: 'tmdb'  
-                                        });  
-                                    });  
-                                }  
-                            });  
-                        }, 500);  
-                    }  
-                });  
-            },  
-  
             toggleActorsButton: function() {  
+                const button = $('.online-cinemas-actors');  
                 if (this.settings.showActors) {  
-                    $('.online-cinemas-actors').show();  
+                    button.removeClass('hide');  
                 } else {  
-                    $('.online-cinemas-actors').hide();  
+                    button.addClass('hide');  
                 }  
             },  
   
             addActorsButton: function() {  
-                const ico = '<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4"><path stroke-linecap="round" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/><path d="M30 24v-4.977C30 16.226 28.136 14 24 14s-6 2.226-6 5.023V24"/><path stroke-linejoin="round" d="M30 24h-6v-4.977C24 16.226 25.864 14 30 14s6 2.226 6 5.023V24h-6Zm-18 0h6v-4.977C24 16.226 22.136 14 18 14s-6 2.226-6 5.023V24h6Z"/></g></svg>';  
+                const ico = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4"><path d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/><path d="M30 24v-4.977C30 16.226 28.136 14 24 14s-6 2.226-6 5.023V24"/><path stroke-linejoin="round" d="M30 24h-6v-4.977C24 16.226 25.864 14 30 14s6 2.226 6 5.023V24h-6Zm-18 0h6v-4.977C24 16.226 22.136 14 18 14s-6 2.226-6 5.023V24h6Z"/></g></svg>';  
                 const button = $(`<li class="menu__item selector online-cinemas-actors" data-action="actors">  
                     <div class="menu__ico">${ico}</div>  
                     <div class="menu__text">Актори</div>  
@@ -112,22 +71,133 @@
             },  
   
             showActors: function() {  
-                Lampa.Activity.push({  
-                    url: "person/popular",  
-                    title: "Актори",  
-                    component: "category_full",  
-                    source: "tmdb",  
-                    page: 1  
+                // Завантажуємо список популярних акторів через API  
+                Lampa.Api.person({url: 'person/popular', page: 1}, (data) => {  
+                    if (data && data.results) {  
+                        // Створюємо активність з власним компонентом  
+                        Lampa.Activity.push({  
+                            url: '',  
+                            title: 'Актори',  
+                            component: 'actors_list',  
+                            persons: data.results,  
+                            source: 'tmdb',  
+                            page: 1  
+                        });  
+                    }  
+                }, () => {  
+                    Lampa.Noty.show('Помилка завантаження акторів');  
                 });  
             }  
         };  
+  
+        // Створюємо власний компонент для відображення акторів  
+        function ActorsListComponent(object) {  
+            let scroll, html, last;  
+            let persons = object.persons || [];  
+            let active = 0;  
+  
+            this.create = function() {  
+                html = Lampa.Template.get('items_line', {title: 'Популярні актори'});  
+                scroll = new Lampa.Scroll({horizontal: false, mask: true, over: true});  
+  
+                let grid = $('<div class="category-full__grid"></div>');  
+  
+                // Створюємо картки акторів, використовуючи логіку з full_persons  
+                persons.forEach((element) => {  
+                    let person = Lampa.Template.get('full_person', {  
+                        name: element.name,  
+                        role: element.known_for_department || Lampa.Lang.translate('title_actor')  
+                    });  
+  
+                    person.on('visible', () => {  
+                        let img = person.find('img')[0];  
+  
+                        img.onerror = function() {  
+                            img.src = './img/actor.svg';  
+                        };  
+  
+                        img.onload = function() {  
+                            person.addClass('full-person--loaded');  
+                        };  
+  
+                        img.src = element.profile_path ?   
+                            Lampa.Api.img(element.profile_path, 'w276_and_h350_face') :   
+                            './img/actor.svg';  
+                    });  
+  
+                    // Використовуємо ту саму логіку, що і в full_persons.js:72-79  
+                    person.on('hover:focus', (e) => {  
+                        last = e.target;  
+                        active = persons.indexOf(element);  
+                        scroll.update($(e.target), true);  
+                    }).on('hover:enter', () => {  
+                        // Явно відкриваємо компонент actor, як у full_persons  
+                        Lampa.Activity.push({  
+                            url: '',  
+                            title: Lampa.Lang.translate('title_persons'),  
+                            component: 'actor',  
+                            id: element.id,  
+                            source: object.source || 'tmdb'  
+                        });  
+                    });  
+  
+                    grid.append(person);  
+                });  
+  
+                scroll.append(grid);  
+                html.find('.items-line__body').append(scroll.render());  
+  
+                this.activity.toggle();  
+            };  
+  
+            this.start = function() {  
+                Lampa.Controller.add('content', {  
+                    toggle: () => {  
+                        Lampa.Controller.collectionSet(scroll.render());  
+                        Lampa.Controller.collectionFocus(last, scroll.render());  
+                    },  
+                    left: () => {  
+                        if (Lampa.Navigator.canmove('left')) Lampa.Navigator.move('left');  
+                        else Lampa.Controller.toggle('menu');  
+                    },  
+                    right: () => {  
+                        Lampa.Navigator.move('right');  
+                    },  
+                    up: () => {  
+                        if (Lampa.Navigator.canmove('up')) Lampa.Navigator.move('up');  
+                        else Lampa.Controller.toggle('head');  
+                    },  
+                    down: () => {  
+                        Lampa.Navigator.move('down');  
+                    },  
+                    back: () => {  
+                        Lampa.Activity.backward();  
+                    }  
+                });  
+  
+                Lampa.Controller.toggle('content');  
+            };  
+  
+            this.pause = function() {};  
+            this.stop = function() {};  
+            this.render = function() {  
+                return html;  
+            };  
+            this.destroy = function() {  
+                scroll.destroy();  
+                html.remove();  
+            };  
+        }  
+  
+        // Реєструємо власний компонент  
+        Lampa.Component.add('actors_list', ActorsListComponent);  
   
         function add() {  
             OnlineCinemas.init();  
             OnlineCinemas.addActorsButton();  
         }  
   
-        if(window.appready) add();  
+        if (window.appready) add();  
         else {  
             Lampa.Listener.follow('app', function (e) {  
                 if (e.type == 'ready') add();  
@@ -135,5 +205,5 @@
         }  
     }  
   
-    if(!window.plugin_online_cinemas_ready) startPlugin();  
+    if (!window.plugin_online_cinemas_ready) startPlugin();  
 })();
