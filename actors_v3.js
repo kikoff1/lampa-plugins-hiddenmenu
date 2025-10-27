9ccfe64582bcd549005a60d1149c5f61f2c4fd0d
kikoff1 <neris54@gmail.com>
2025-10-27 09:23:31 +0200

    Update actors_v3.js

--- actors_v3.js
+++ actors_v3.js
@@ -13,6 +13,7 @@
             init: function() {  
                 this.loadSettings();  
                 this.createSettings();  
+                this.addActorsButton();  
                 this.initStorageListener();  
             },  
   
@@ -23,10 +24,8 @@
             createSettings: function() {  
                 Lampa.SettingsApi.addComponent({  
                     component: 'online_cinemas',  
-                    icon: `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="512" height="512" viewBox="0 0 20 20">  
-                        <path fill="currentColor" fill-rule="evenodd" d="M10 10c-2.216 0-4.019-1.794-4.019-4S7.783 2 10 2s4.019 1.794 4.019 4-1.802 4-4.019 4zm3.776.673a5.978 5.978 0 0 0 2.182-5.603C15.561 2.447 13.37.348 10.722.042 7.07-.381 3.972 2.449 3.972 6c0 1.89.88 3.574 2.252 4.673C2.852 11.934.39 14.895.004 18.891A1.012 1.012 0 0 0 1.009 20a.99.99 0 0 0 .993-.891C2.404 14.646 5.837 12 10 12s7.596 2.646 7.999 7.109a.99.99 0 0 0 .993.891 1.012 1.012 0 0 0 1.005-1.109c-.386-3.996-2.848-6.957-6.221-8.218z"/>  
-                    </svg>`,  
-                    name: 'Популярні актори'  
+                    name: 'Популярні актори',  
+                    icon: '<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="18" cy="12" r="5" stroke="currentColor" stroke-width="2"/><path d="M8 28c0-5.523 4.477-10 10-10s10 4.477 10 10" stroke="currentColor" stroke-width="2" stroke-linecap="round"/></svg>'  
                 });  
   
                 Lampa.SettingsApi.addParam({  
@@ -44,185 +43,154 @@
             },  
   
             initStorageListener: function() {  
-                Lampa.Storage.listener.follow('change', (e) => {  
-                    if (e.name === 'show_actors') {  
-                        this.settings.showActors = e.value;  
+                Lampa.Storage.listener.follow('change', (event) => {  
+                    if (event.name === 'show_actors') {  
+                        this.settings.showActors = event.value;  
                         this.toggleActorsButton();  
                     }  
                 });  
             },  
   
-            toggleActorsButton: function() {  
-                const button = $('.menu__item[data-action="actors"]');  
-                if (this.settings.showActors) {  
-                    button.removeClass('hidden');  
-                } else {  
-                    button.addClass('hidden');  
-                }  
-            },  
-  
             addActorsButton: function() {  
-                const ico = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="512" height="512" viewBox="0 0 20 20">  
-                    <path fill="currentColor" fill-rule="evenodd" d="M10 10c-2.216 0-4.019-1.794-4.019-4S7.783 2 10 2s4.019 1.794 4.019 4-1.802 4-4.019 4zm3.776.673a5.978 5.978 0 0 0 2.182-5.603C15.561 2.447 13.37.348 10.722.042 7.07-.381 3.972 2.449 3.972 6c0 1.89.88 3.574 2.252 4.673C2.852 11.934.39 14.895.004 18.891A1.012 1.012 0 0 0 1.009 20a.99.99 0 0 0 .993-.891C2.404 14.646 5.837 12 10 12s7.596 2.646 7.999 7.109a.99.99 0 0 0 .993.891 1.012 1.012 0 0 0 1.005-1.109c-.386-3.996-2.848-6.957-6.221-8.218z"/>  
-                </svg>`;  
+                if (!this.settings.showActors) return;  
   
-                const button = $(`<li class="menu__item selector" data-action="actors">  
-                    <div class="menu__ico">${ico}</div>  
+                let button = $(`<li class="menu__item selector">  
+                    <div class="menu__ico">  
+                        <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">  
+                            <circle cx="18" cy="12" r="5" stroke="currentColor" stroke-width="2"/>  
+                            <path d="M8 28c0-5.523 4.477-10 10-10s10 4.477 10 10" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>  
+                        </svg>  
+                    </div>  
                     <div class="menu__text">Актори</div>  
                 </li>`);  
   
-                button.on('hover:enter', this.showActors.bind(this));  
-                $('.menu .menu__list').eq(0).append(button);  
-                this.toggleActorsButton();  
+                button.on('hover:enter', () => {  
+                    Lampa.Activity.push({  
+                        url: 'person/popular',  
+                        title: 'Популярні актори',  
+                        component: 'actors_list',  
+                        source: 'tmdb',  
+                        page: 1  
+                    });  
+                });  
+  
+                let historyItem = $('.menu .menu__list').eq(0).find('[data-action="history"]').parent();  
+                if (historyItem.length) {  
+                    button.insertBefore(historyItem);  
+                } else {  
+                    $('.menu .menu__list').eq(0).append(button);  
+                }  
             },  
   
-            showActors: function() {  
-                Lampa.Activity.push({  
-                    url: 'person/popular',  
-                    title: 'Актори',  
-                    component: 'actors_list',  
-                    source: 'tmdb',  
-                    page: 1  
-                });  
+            toggleActorsButton: function() {  
+                $('.menu .menu__item').filter(function() {  
+                    return $(this).find('.menu__text').text() === 'Актори';  
+                }).remove();  
+  
+                if (this.settings.showActors) {  
+                    this.addActorsButton();  
+                }  
             }  
         };  
   
         function ActorsListComponent(object) {  
-            let network = new Lampa.Reguest();  
-            let scroll = new Lampa.Scroll({  
-                mask: true,  
-                over: true,  
-                step: 250  
-            });  
-              
-            let items = [];  
-            let html;  
-            let body;  
+            let html, scroll, items = [], network = new Lampa.Reguest();  
             let active = 0;  
-            let last;  
   
             this.create = function() {  
-                // Додаємо клас category-full до body для правильного CSS  
-                $('body').addClass('category-full');  
+                html = document.createElement('div');  
+                html.className = 'category-full';  
+  
+                scroll = new Lampa.Scroll({  
+                    mask: true,  
+                    over: true  
+                });  
+  
+                scroll.render().addClass('category-full');  
                   
-                html = $('<div></div>');  
-                body = $('<div class="category-full"></div>');  
-                  
-                // Використовуємо правильну структуру scroll  
-                scroll.render().addClass('layer--wheight');  
-                scroll.minus(body);  
-                  
-                html.append(scroll.render());  
+                html.appendChild(scroll.render()[0]);  
+  
+                this.activity.loader(true);  
   
                 this.loadActors();  
             };  
   
             this.loadActors = function() {  
-                network.silent('https://api.themoviedb.org/3/person/popular?api_key=4ef0d7355d9ffb5151e987764708ce96&language=uk-UA&page=' + (object.page || 1), (data) => {  
-                    if (data.results && data.results.length) {  
-                        this.append(data);  
-                          
-                        if (data.page < data.total_pages) {  
-                            this.more(data);  
-                        }  
-                          
-                        // Оновлюємо Layer після додавання карток  
-                        Lampa.Layer.update(scroll.render());  
-                        Lampa.Layer.visible(scroll.render());  
-                    } else {  
-                        scroll.append(Lampa.Template.get('list_empty'));  
-                    }  
+                Lampa.Api.list({  
+                    url: 'person/popular',  
+                    page: object.page || 1,  
+                    source: 'tmdb'  
+                }, (data) => {  
+                    this.append(data);  
+                    this.activity.loader(false);  
+                    this.activity.toggle();  
                 }, () => {  
-                    scroll.append(Lampa.Template.get('error', {title: 'Помилка завантаження'}));  
+                    this.empty();  
                 });  
             };  
   
             this.append = function(data) {  
-                data.results.forEach((person) => {  
-                    let card = Lampa.Template.get('card', {  
-                        title: person.name,  
-                        release_year: ''  
+                if (!data.results || !data.results.length) {  
+                    this.empty();  
+                    return;  
+                }  
+  
+                data.results.forEach((element) => {  
+                    let card = new Lampa.Card(element, {  
+                        card_category: true  
                     });  
   
-                    card.addClass('card--category');  
-  
-                    card.find('.card__img').attr('src', person.profile_path ?   
-                        'https://image.tmdb.org/t/p/w300' + person.profile_path :   
-                        './img/actor.svg'  
-                    );  
-  
-                    card.on('hover:focus', () => {  
-                        last = card[0];  
-                        active = items.indexOf(card);  
-                        scroll.update(card, true);  
-                    });  
-  
-                    // Явно встановлюємо відкриття компонента actor  
-                    card.on('hover:enter', () => {  
+                    card.onEnter = () => {  
                         Lampa.Activity.push({  
                             url: '',  
-                            title: person.name,  
                             component: 'actor',  
-                            id: person.id,  
-                            source: 'tmdb'  
+                            id: element.id,  
+                            source: object.source || 'tmdb'  
                         });  
-                    });  
+                    };  
   
-                    scroll.append(card);  
+                    card.create();  
                     items.push(card);  
+                    scroll.append(card.render());  
                 });  
+  
+                Lampa.Controller.collectionSet(scroll.render());  
+                Lampa.Controller.collectionFocus(items[0] ? items[0].render() : false, scroll.render());  
             };  
   
-            this.more = function(data) {  
-                let more = $('<div class="category-full__more selector"><span>Показати більше</span></div>');  
-                  
-                more.on('hover:focus', () => {  
-                    last = more[0];  
-                    scroll.update(more, true);  
-                });  
-  
-                more.on('hover:enter', () => {  
-                    more.remove();  
-                    object.page++;  
-                    this.loadActors();  
-                });  
-  
-                scroll.append(more);  
-                items.push(more);  
-            };  
-  
-            this.background = function() {  
-                Lampa.Background.immediately('');  
+            this.empty = function() {  
+                let empty = new Lampa.Empty();  
+                scroll.append(empty.render());  
+                this.activity.loader(false);  
+                this.activity.toggle();  
             };  
   
             this.start = function() {  
                 Lampa.Controller.add('content', {  
                     toggle: () => {  
                         Lampa.Controller.collectionSet(scroll.render());  
-                        Lampa.Controller.collectionFocus(last || false, scroll.render());  
+                        Lampa.Controller.collectionFocus(items[0] ? items[0].render() : false, scroll.render());  
                     },  
                     left: () => {  
-                        if (Navigator.canmove('left')) Navigator.move('left');  
+                        if (Lampa.Navigator.canmove('left')) Lampa.Navigator.move('left');  
                         else Lampa.Controller.toggle('menu');  
                     },  
-                    down: this.down,  
-                    up: this.up,  
-                    back: this.back  
+                    right: () => {  
+                        Lampa.Navigator.move('right');  
+                    },  
+                    up: () => {  
+                        if (Lampa.Navigator.canmove('up')) Lampa.Navigator.move('up');  
+                        else Lampa.Controller.toggle('head');  
+                    },  
+                    down: () => {  
+                        Lampa.Navigator.move('down');  
+                    },  
+                    back: () => {  
+                        Lampa.Activity.backward();  
+                    }  
                 });  
   
                 Lampa.Controller.toggle('content');  
-            };  
-  
-            this.down = function() {  
-                Navigator.move('down');  
-            };  
-  
-            this.up = function() {  
-                Navigator.move('up');  
-            };  
-  
-            this.back = function() {  
-                Lampa.Activity.backward();  
             };  
   
             this.pause = function() {};  
@@ -236,7 +204,6 @@
             this.destroy = function() {  
                 network.clear();  
                 scroll.destroy();  
-                $('body').removeClass('category-full');  
                 if (html) html.remove();  
             };  
         }  
@@ -245,12 +212,11 @@
   
         function add() {  
             OnlineCinemas.init();  
-            OnlineCinemas.addActorsButton();  
         }  
   
         if (window.appready) add();  
         else {  
-            Lampa.Listener.follow('app', function (e) {  
+            Lampa.Listener.follow('app', function(e) {  
                 if (e.type == 'ready') add();  
             });  
         }  
