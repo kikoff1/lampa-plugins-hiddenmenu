dd43158f0502f48d3b3ed170a7dfb7224129d706
kikoff1 <neris54@gmail.com>
2025-10-29 13:03:53 +0200

    Update actors_v3.js

--- actors_v3.js
+++ actors_v3.js
@@ -1,5 +1,9 @@
 (function () {  
   
+
+
+
+
     function Actors() {  
         let scroll = new Lampa.Scroll({ mask: true })  
         let body = document.createElement('div')  
@@ -22,6 +26,8 @@
                 if (!json.results || !json.results.length) {  
                     let empty = new Lampa.Empty()  
                     body.appendChild(empty.render(true))  
+                    this.start = empty.start  
+                    this.activity.toggle()  
                     return  
                 }  
   
@@ -32,13 +38,16 @@
                         title: person.name,  
                         poster_path: person.profile_path,  
                         profile_path: person.profile_path,  
-                        gender: person.gender || 2  
+                        gender: person.gender,  
+                        known_for_department: person.known_for_department  
                     }  
   
                     let card = new Lampa.Card(cardData, {  
                         card_category: true,  
                         object: { source: 'tmdb' }  
                     })  
+  
+                    card.create()  
   
                     card.onEnter = () => {  
                         Lampa.Activity.push({  
@@ -50,9 +59,18 @@
                         })  
                     }  
   
-                    body.appendChild(card.render(true)) 
+                    card.onFocus = (target) => {  
+                        last = target  
+                        active = items.indexOf(card)  
+                        scroll.update(card.render(true))  
+                    }  
+  
+                    body.appendChild(card.render(true))  
                     items.push(card)  
                 })  
+  
+                scroll.append(body)  
+                this.html.appendChild(scroll.render(true))  
   
                 setTimeout(() => {  
                     Lampa.Layer.visible(scroll.render(true))  
@@ -63,37 +81,30 @@
                 this.activity.loader(false)  
                 let empty = new Lampa.Empty()  
                 body.appendChild(empty.render(true))  
+                this.start = empty.start  
+                this.activity.toggle()  
             })  
         }  
   
         this.start = function () {  
             Lampa.Controller.add('content', {  
                 toggle: () => {  
-                    Lampa.Controller.collectionSet(scroll.render(true))  
-                    Lampa.Controller.collectionFocus(last, scroll.render(true))  
+                    Lampa.Controller.collectionSet(this.html)  
+                    Lampa.Controller.collectionFocus(last, this.html)  
                 },  
-                left: () => {  
-                    if (Lampa.Navigator.canmove('left')) Lampa.Navigator.move('left')  
-                    else Lampa.Controller.toggle('menu')  
+                up: () => {  
+                    if (Lampa.Navigator.canmove('up')) Lampa.Navigator.move('up')  
+                    else Lampa.Controller.toggle('head')  
+                },  
+                down: () => {  
+                    Lampa.Navigator.move('down')  
                 },  
                 right: () => {  
                     Lampa.Navigator.move('right')  
                 },  
-                up: () => {  
-                    if (active > 0) {  
-                        active--  
-                        scroll.update(items[active].render(true))  
-                    } else if (Lampa.Navigator.canmove('up')) {  
-                        Lampa.Navigator.move('up')  
-                    }  
-                },  
-                down: () => {  
-                    if (active < items.length - 1) {  
-                        active++  
-                        scroll.update(items[active].render(true))  
-                    } else if (Lampa.Navigator.canmove('down')) {  
-                        Lampa.Navigator.move('down')  
-                    }  
+                left: () => {  
+                    if (Lampa.Navigator.canmove('left')) Lampa.Navigator.move('left')  
+                    else Lampa.Controller.toggle('menu')  
                 },  
                 back: () => {  
                     Lampa.Activity.backward()  
@@ -103,39 +114,33 @@
             Lampa.Controller.toggle('content')  
         }  
   
-        scroll.onWheel = (step) => {  
-            if (step > 0) {  
-                if (active < items.length - 1) {  
-                    active++  
-                    scroll.update(items[active].render(true))  
-                }  
-            } else {  
-                if (active > 0) {  
-                    active--  
-                    scroll.update(items[active].render(true))  
-                }  
-            }  
-        }  
+        this.pause = function () {}  
   
-        scroll.append(body)  
+        this.stop = function () {}  
   
         this.render = function () {  
-            return scroll.render(true)  
+            return this.html  
         }  
   
         this.destroy = function () {  
             scroll.destroy()  
-            body.remove()  
+            this.html.remove()  
         }  
     }  
   
     function startPlugin() {  
+        const manifest = {  
+            type: 'content',  
+            version: '1.0.8',  
+            name: 'Actors',  
+            description: 'Популярні актори з TMDB',  
+            component: 'actors_list'  
+        }  
+  
         // Додаємо CSS стилі для компактних карток  
         $('<style>')  
             .text(`  
                 .category-full {  
-                    display: flex !important;  
-                    flex-wrap: wrap !important;  
                     isolation: isolate;  
                 }  
                   
@@ -151,28 +156,18 @@
                     max-height: 3.6em;  
                     overflow: hidden;  
                     line-height: 1.2;  
-                    text-overflow: ".";  
+                    text-overflow: ellipsis;  
                     display: -webkit-box;  
                     -webkit-line-clamp: 3;  
                     -webkit-box-orient: vertical;  
                 }  
-                  
-                .category-full .card--category .card__view {  
-                    margin-bottom: 0.5em;  
-                }  
             `)  
             .appendTo('head')  
   
-        const manifest = {  
-            type: 'content',  
-            version: '1.0.8',  
-            name: 'Actors',  
-            description: 'Популярні актори з TMDB',  
-            component: 'actors_list'  
-        }  
-  
+        // Реєстрація компонента  
         Lampa.Component.add('actors_list', Actors)  
   
+        // Переклади  
         Lampa.Lang.add({  
             title_actors: {  
                 uk: 'Актори',  
@@ -181,15 +176,16 @@
             }  
         })  
   
+        // Реєстрація плагіна  
         Lampa.Manifest.plugins = manifest  
   
         function addMenuButton() {  
-            let ico = `<svg height="260" viewBox="0 0 244 260" fill="none" xmlns="http://www.w3.org/2000/svg">  
-                <path d="M122 0C54.6538 0 0 54.6538 0 122C0 189.346 54.6538 244 122 244C189.346 244 244 189.346 244 122C244 54.6538 189.346 0 122 0ZM122 183.385C93.8462 183.385 60.6154 183.385 60.6154 122C60.6154 60.6154 93.8462 60.6154 122 60.6154C150.154 60.6154 183.385 60.6154 183.385 122C183.385 183.385 150.154 183.385 122 183.385Z" fill="white"/>  
-            </svg>`  
-  
-            let button = $(`<li class="menu__item selector">  
-                <div class="menu__ico">${ico}</div>  
+            let button = $(`<li class="menu__item selector" data-action="actors">  
+                <div class="menu__ico">  
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">  
+                        <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>  
+                    </svg>  
+                </div>  
                 <div class="menu__text">${Lampa.Lang.translate('title_actors')}</div>  
             </li>`)  
   
