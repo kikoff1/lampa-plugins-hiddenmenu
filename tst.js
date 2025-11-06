b4f416c2d145ab891f26752f3f8abec88d66d172
kikoff1 <neris54@gmail.com>
2025-11-06 15:09:22 +0200

    Update tst.js

--- tst.js
+++ tst.js
@@ -16,7 +16,8 @@
             },  
             onChange: function(value) {  
                 if (value == true) {  
-                    Lampa.Template.add('hide_nav_bar', '<style id="hide_nav_bar">.navigation-bar{display:none!important}</style>');  
+                    $('#hide_nav_bar').remove();  
+                    Lampa.Template.add('hide_nav_bar', '<style id="hide_nav_bar">body.true--mobile .navigation-bar{display:none!important}</style>');  
                     $('body').append(Lampa.Template.get('hide_nav_bar', {}, true));  
                 }  
                 if (value == false) {  
@@ -30,7 +31,7 @@
   
         // Застосовуємо налаштування при запуску  
         if (Lampa.Storage.field('hide_navigation_bar') == true) {  
-            Lampa.Template.add('hide_nav_bar', '<style id="hide_nav_bar">.navigation-bar{display:none!important}</style>');  
+            Lampa.Template.add('hide_nav_bar', '<style id="hide_nav_bar">body.true--mobile .navigation-bar{display:none!important}</style>');  
             $('body').append(Lampa.Template.get('hide_nav_bar', {}, true));  
         }  
     }  
