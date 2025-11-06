62233f6524e15d15bae2af5607b81a306d833ff0
kikoff1 <neris54@gmail.com>
2025-11-06 15:21:05 +0200

    Update tst.js

--- tst.js
+++ tst.js
@@ -12,35 +12,22 @@
             },  
             field: {  
                 name: 'Приховати панель навігації',  
-                description: 'Приховує нижню панель навігації (Назад, Головна, Пошук, Налаштування)'  
+                description: 'Приховує нижню панель навігації'  
             },  
             onChange: function(value) {  
-                if (value == true) {  
-                    $('#hide_nav_bar').remove();  
-                    Lampa.Template.add('hide_nav_bar', '<style id="hide_nav_bar">body.true--mobile .navigation-bar{display:none!important}</style>');  
+                if (Lampa.Storage.field('hide_navigation_bar') == true) {  
+                    Lampa.Template.add('hide_nav_bar', '<style id="hide_nav_bar">.navigation-bar{display:none!important}</style>');  
                     $('body').append(Lampa.Template.get('hide_nav_bar', {}, true));  
                 }  
-                if (value == false) {  
+                if (Lampa.Storage.field('hide_navigation_bar') == false) {  
                     $('#hide_nav_bar').remove();  
-                      
-                    // КЛЮЧОВА ЗМІНА: примусово оновлюємо стилі  
-                    var $nav = $('.navigation-bar');  
-                    if ($nav.length > 0) {  
-                        // Примусовий reflow  
-                        $nav[0].offsetHeight;  
-                        // Тригеруємо перерахунок стилів  
-                        $nav.hide().show();  
-                    }  
                 }  
-                  
-                // Зберігаємо стан  
-                Lampa.Storage.set('hide_navigation_bar', value);  
             }  
         });  
   
-        // Застосовуємо налаштування при запуску  
+        // Застосовуємо при запуску  
         if (Lampa.Storage.field('hide_navigation_bar') == true) {  
-            Lampa.Template.add('hide_nav_bar', '<style id="hide_nav_bar">body.true--mobile .navigation-bar{display:none!important}</style>');  
+            Lampa.Template.add('hide_nav_bar', '<style id="hide_nav_bar">.navigation-bar{display:none!important}</style>');  
             $('body').append(Lampa.Template.get('hide_nav_bar', {}, true));  
         }  
     }  
