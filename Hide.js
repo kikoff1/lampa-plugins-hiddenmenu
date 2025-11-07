eb35deb8c6ed20bdd3b354c695594995868c563f
kikoff1 <neris54@gmail.com>
2025-11-07 15:08:21 +0200

    Update Hide.js

--- Hide.js
+++ Hide.js
@@ -4,7 +4,7 @@
     function startPlugin() {  
         let manifest = {  
             type: 'other',  
-            version: '1.0.8',  
+            version: '1.0.9',  
             name: 'Розширений редактор меню',  
             description: 'Редагування всіх пунктів меню з можливістю сортування та приховування',  
         }  
@@ -18,20 +18,6 @@
         let active_controller = 'menu'  
         let settings_added = false  
   
-        // Мапа назв для верхнього меню  
-        function getHeadMenuNames() {  
-            return {  
-                'open--search': Lampa.Lang.translate('search'),  
-                'open--premium': 'Premium',  
-                'full--screen': Lampa.Lang.translate('player_full_screen'),  
-                'open--notice': Lampa.Lang.translate('title_notice'),  
-                'open--profile': Lampa.Lang.translate('title_profile'),  
-                'open--feed': Lampa.Lang.translate('menu_feed') || 'Feed',  
-                'settings': Lampa.Lang.translate('menu_settings') || 'Settings'  
-            }  
-        }  
-  
-        // Функція для видалення стандартного пункту "Редагувати"  
         function removeEditMenuItem() {  
             $('.menu__item[data-action="edit"]').remove()  
         }  
@@ -39,16 +25,13 @@
         function init() {  
             items_map = []  
               
-            // Збираємо всі пункти з обох секцій лівого меню  
             $('.menu .menu__list').each(function() {  
                 $(this).find('.menu__item').each(function() {  
                     items_map.push($(this))  
                 })  
             })  
   
-            // Видаляємо стандартний пункт "Редагувати"  
             removeEditMenuItem()  
-  
             observe()  
         }  
   
@@ -67,38 +50,19 @@
         function initSettings() {  
             settings_items_map = []  
               
-            // Збираємо компоненти з основного меню налаштувань  
-            $('.settings-folder.selector').each(function() {  
+            // Збираємо компоненти безпосередньо з DOM  
+            $('.settings .settings-folder.selector').each(function() {  
                 settings_items_map.push($(this))  
             })  
         }  
   
-        function getHeadItemName(item) {  
-            const headMenuNames = getHeadMenuNames()  
-              
-            // Перевіряємо всі можливі класи  
-            for (let className in headMenuNames) {  
-                if (item.hasClass(className)) {  
-                    return headMenuNames[className]  
-                }  
-            }  
-              
-            // Перевіряємо data-атрибути  
-            let name = item.attr('data-title') ||   
-                      item.find('[data-title]').attr('data-title') ||  
-                      item.attr('title')  
-              
-            if (name) return name  
-              
-            // Перевіряємо текстовий вміст  
-            let text = item.text().trim()  
-            if (text && text.length > 0 && text.length < 50) return text  
-              
-            return 'Пункт меню'  
-        }  
-  
         function start(type = 'menu') {  
             active_controller = Lampa.Controller.enabled().name  
+              
+            // Для налаштувань збираємо пункти прямо перед відкриттям  
+            if(type === 'settings') {  
+                initSettings()  
+            }  
               
             let list = $('<div class="menu-edit-list"></div>')  
             let current_items = type === 'head' ? head_items_map : type === 'settings' ? settings_items_map : items_map  
@@ -115,11 +79,12 @@
                 let item_icon = null  
                   
                 if(type === 'head') {  
-                    item_title = getHeadItemName(item_orig)  
+                    // Для верхнього меню не показуємо підписи, тільки іконки  
+                    item_title = ''  
                     item_icon = item_clone.find('svg').first()  
                 } else if(type === 'settings') {  
                     item_title = item_clone.find('.settings-folder__name').text().trim() || 'Налаштування'  
-                    item_icon = item_clone.find('.settings-folder__icon svg').first()  
+                    item_icon = item_clone.find('svg').first()  
                 } else {  
                     item_title = item_clone.find('.menu__text').text()  
                     item_icon = item_clone.find('.menu__ico')  
@@ -147,13 +112,9 @@
                 </div>`)  
   
                 if(type === 'menu') {  
-                    item_sort.find('.menu-edit-list__icon').append(item_icon.html())  
-                } else if(type === 'head') {  
-                    if(item_icon.length > 0) {  
-                        item_sort.find('.menu-edit-list__icon').append(item_icon.clone())  
-                    }  
-                } else if(type === 'settings') {  
-                    if(item_icon.length > 0) {  
+                    item_sort.find('.menu-edit-list__icon').append(item_clone.find('.menu__ico').html())  
+                } else if(type === 'head' || type === 'settings') {  
+                    if(item_icon && item_icon.length) {  
                         item_sort.find('.menu-edit-list__icon').append(item_icon.clone())  
                     }  
                 }  
@@ -198,7 +159,10 @@
                 onBack: () => {  
                     Lampa.Modal.close()  
                     save(type)  
-                    Lampa.Controller.toggle(active_controller)  
+                      
+                    if(active_controller) {  
+                        Controller.toggle(active_controller)  
+                    }  
                 }  
             })  
         }  
@@ -224,11 +188,9 @@
               
             if (items.length) {  
                 let container = $('.head__actions')  
-                items.forEach((item) => {  
-                    let el = container.children().filter(function() {  
-                        return getHeadItemName($(this)) === item  
-                    })  
-                    if (el.length) el.appendTo(container)  
+                items.forEach((itemIndex) => {  
+                    let el = head_items_map[itemIndex]  
+                    if (el && el.length) el.appendTo(container)  
                 })  
             }  
         }  
@@ -275,10 +237,9 @@
             $('.head__actions').children().removeClass('hidden')  
   
             if (items.length) {  
-                items.forEach((item) => {  
-                    $('.head__actions').children().filter(function() {  
-                        return getHeadItemName($(this)) === item  
-                    }).addClass('hidden')  
+                items.forEach((itemIndex) => {  
+                    let el = head_items_map[itemIndex]  
+                    if (el && el.length) el.addClass('hidden')  
                 })  
             }  
         }  
@@ -303,19 +264,25 @@
             let current_items = type === 'head' ? head_items_map : type === 'settings' ? settings_items_map : items_map  
             let storage_prefix = type === 'head' ? 'head' : type === 'settings' ? 'settings' : 'menu'  
   
-            current_items.forEach(function(item) {  
+            current_items.forEach(function(item, index) {  
                 let name = ''  
                 if(type === 'menu') {  
                     name = item.find('.menu__text').text().trim()  
+                    sort.push(name)  
                 } else if(type === 'head') {  
-                    name = getHeadItemName(item)  
+                    // Для верхнього меню зберігаємо індекси  
+                    sort.push(index)  
                 } else if(type === 'settings') {  
                     name = item.find('.settings-folder__name').text().trim()  
+                    sort.push(name)  
                 }  
                   
-                sort.push(name)  
                 if (item.hasClass('hidden')) {  
-                    hide_items.push(name)  
+                    if(type === 'head') {  
+                        hide_items.push(index)  
+                    } else {  
+                        hide_items.push(name)  
+                    }  
                 }  
             })  
   
@@ -329,14 +296,14 @@
             if(type === 'menu') {  
                 order()  
                 hide()  
-                // Видаляємо стандартний пункт після оновлення  
                 removeEditMenuItem()  
             } else if(type === 'head') {  
                 orderHead()  
                 hideHead()  
             } else if(type === 'settings') {  
                 orderSettings()  
-                hideSettings              }  
+                hideSettings()  
+            }  
         }  
   
         function observe() {  
@@ -361,14 +328,13 @@
         }  
   
         function addSettingsItem() {  
-            // Перевіряємо, чи вже додано компонент  
             if(settings_added) return  
               
             Lampa.SettingsApi.addComponent({  
                 component: 'menu_editor',  
                 name: 'Редагування меню',  
                 icon: `<svg width="30" height="29" viewBox="0 0 30 29" fill="none" xmlns="http://www.w3.org/2000/svg">  
-                    <path d="M18.2989 5.27973L2.60834 20.9715C2.52933 21.0507 2.47302 21.1496 2.44528 21.258L0.706081 28.2386C0.680502 28.3422 0.682069 28.4507 0.710632 28.5535C0.739195 28.6563 0.793788 28.75 0.869138 28.8255C0.984875 28.9409 1.14158 29.0057 1.30498 29.0059C1.35539 29.0058 1.4056 28.9996 1.45449 28.9873L8.43509 27.2479C8.54364 27.2206 8.64271 27.1643 8.72172 27.0851L24.4137 11.3944L18.2989 5.27973ZM28.3009 3.14018L26.5543 1.39363C25.3869 0.226285 23.3524 0.227443 22.1863 1.39363L20.0469 3.53318L26.1614 9.64766L28.3009 7.50816C28.884 6.9253 29.2052 6.14945 29.2052 5.32432C29.2052 4.49919 28.884 3.72333 28.3009 3.14018Z" fill="currentColor"/>  
+                    <path d="M18.2989 5.27973L2.60834 20.9715C2.52933 21.0507 2.47302 21.1496 2.44528 21.258L0.706081 28.2386C0.680502 28.3422 0.682069 28.4507 0.710632 28.5535C0.739195 28.6 563 0.793788 28.75 0.869138 28.8255C0.984875 28.9409 1.14158 29.0057 1.30498 29.0059C1.35539 29.0058 1.4056 28.9996 1.45449 28.9873L8.43509 27.2479C8.54364 27.2206 8.64271 27.1643 8.72172 27.0851L24.4137 11.3944L18.2989 5.27973ZM28.3009 3.14018L26.5543 1.39363C25.3869 0.226285 23.3524 0.227443 22.1863 1.39363L20.0469 3.53318L26.1614 9.64766L28.3009 7.50816C28.884 6.9253 29.2052 6.14945 29.2052 5.32432C29.2052 4.49919 28.884 3.72333 28.3009 3.14018Z" fill="currentColor"/>  
                 </svg>`  
             })  
   
