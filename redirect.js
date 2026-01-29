ef1f94ceadb6ca211b8d72448f07d6e9e6e00540
kikoff1 <neris54@gmail.com>
2026-01-29 16:01:40 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,77 +1,132 @@
-(function() {   
-    'use strict';   
-    Lampa.Platform.tv();   
-      
-    // Додаємо локалізацію  
-    Lampa.Lang.add({  
-        location_redirect_title: {  
-            ru: 'Смена сервера',  
-            uk: 'Зміна сервера',  
-            en: 'Change server'  
+(function() {     
+    'use strict';     
+    Lampa.Platform.tv();     
+        
+    // Додаємо локалізацію    
+    Lampa.Lang.add({    
+        location_redirect_title: {    
+            ru: 'Смена сервера',    
+            uk: 'Зміна сервера',    
+            en: 'Change server'    
+        },    
+        location_redirect_current: {    
+            ru: 'Текущий',    
+            uk: 'Поточний',    
+            en: 'Current'    
+        },    
+        location_redirect_select_domain: {    
+            ru: 'Выберите домен Lampa',    
+            uk: 'Виберіть домен Lampa',    
+            en: 'Choose Lampa domain'    
         },  
-        location_redirect_current: {  
-            ru: 'Текущий',  
-            uk: 'Поточний',  
-            en: 'Current'  
+        location_redirect_add_custom: {  
+            ru: 'Добавить свой сервер',  
+            uk: 'Додати свій сервер',   
+            en: 'Add custom server'  
         },  
-        location_redirect_select_domain: {  
-            ru: 'Выберите домен Lampa',  
-            uk: 'Виберіть домен Lampa',  
-            en: 'Choose Lampa domain'  
+        location_redirect_custom_server: {  
+            ru: 'Введите адрес сервера',  
+            uk: 'Введіть адресу сервера',  
+            en: 'Enter server address'  
         }  
-    });  
-      
-    var icon_server_redirect = `<svg height="36" viewBox="0 0 38 36" fill="none" xmlns="http://www.w3.org/2000/svg">  
-        <path d="M19 2L24 7L19 12" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>  
-        <path d="M19 24L24 29L19 34" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>  
-        <path d="M14 7H8C5.79086 7 4 8.79086 4 11V25C4 27.2091 5.79086 29 8 29H14" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>  
-        <path d="M24 7H30C32.2091 7 34 8.79086 34 11V25C34 27.2091 32.2091 29 30 29H24" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>  
-        <circle cx="19" cy="18" r="3" stroke="white" stroke-width="2"/>  
-    </svg>`;  
-      
-    function startMe() {   
-        if (window.location.search!='?redirect=1') {   
-            if(window.location.hostname!=Lampa.Storage.get('location_server')) {   
-                if (Lampa.Storage.get('location_server')!='-' && Lampa.Storage.get('location_server')!='')   
-                    window.location.href = 'http://'+Lampa.Storage.get('location_server')+'?redirect=1';   
-            }   
-        } else {   
-            Lampa.Storage.set('location_server','-');   
-        }   
+    });     
+        
+    var icon_server_redirect = `<svg height="36" viewBox="0 0 38 36" fill="none" xmlns="http://www.w3.org/2000/svg">    
+        <path d="M19 2L24 7L19 12" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>    
+        <path d="M19 24L24 29L19 34" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>    
+        <path d="M14 7H8C5.79086 7 4 8.79086 4 11V25C4 27.2091 5.79086 29 8 29H14" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>    
+        <path d="M24 7H30C32.2091 7 34 8.79086 34 11V25C34 27.2091 32.2091 29 30 29H24" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>    
+        <circle cx="19" cy="18" r="3" stroke="white" stroke-width="2"/>    
+    </svg>`;    
+        
+    function getServerValues() {  
+        let values = {     
+            '-': Lampa.Lang.translate('location_redirect_current'),     
+            'lampaua.mooo.com': 'lampaua.mooo.com',     
+            'lampa.mx': 'lampa.mx'     
+        };  
           
-        Lampa.SettingsApi.addComponent({   
-            component: 'location_redirect',   
-            name: Lampa.Lang.translate('location_redirect_title'),   
-            icon: icon_server_redirect   
-        });   
+        // Додаємо кастомні сервери  
+        let customServers = Lampa.Storage.get('location_custom_servers', []);  
+        customServers.forEach(server => {  
+            values[server] = server;  
+        });  
           
-        Lampa.SettingsApi.addParam({   
-            component: 'location_redirect',   
-            param: {   
-                name: 'location_server',   
-                type: 'select',   
-                values: {   
-                    '-': Lampa.Lang.translate('location_redirect_current'),   
-                    'lampaua.mooo.com': 'lampaua.mooo.com',   
-                    'lampa.mx': 'lampa.mx'   
-                },   
-                default: '-'   
-            },   
-            field: {   
-                name: Lampa.Lang.translate('location_redirect_select_domain')   
-            },   
-            onChange: function (value) {   
-                startMe();   
-            }   
-        });   
-    }   
-      
-    if(window.appready) startMe();   
-    else {   
-        Lampa.Listener.follow('app', function(e) {   
-            if(e.type == 'ready') {   
-                startMe();   
-            }   
-        });   
-    }   
+        // Додаємо опцію для додавання нового сервера  
+        values['add_custom'] = Lampa.Lang.translate('location_redirect_add_custom');  
+          
+        return values;  
+    }  
+        
+    function startMe() {     
+        if (window.location.search!='?redirect=1') {     
+            if(window.location.hostname!=Lampa.Storage.get('location_server')) {     
+                if (Lampa.Storage.get('location_server')!='-' && Lampa.Storage.get('location_server')!='')     
+                    window.location.href = 'http://'+Lampa.Storage.get('location_server')+'?redirect=1';     
+            }     
+        } else {     
+            Lampa.Storage.set('location_server','-');     
+        }     
+            
+        Lampa.SettingsApi.addComponent({     
+            component: 'location_redirect',     
+            name: Lampa.Lang.translate('location_redirect_title'),     
+            icon: icon_server_redirect     
+        });     
+            
+        Lampa.SettingsApi.addParam({     
+            component: 'location_redirect',     
+            param: {     
+                name: 'location_server',     
+                type: 'select',     
+                values: getServerValues(),     
+                default: '-'     
+            },     
+            field: {     
+                name: Lampa.Lang.translate('location_redirect_select_domain')     
+            },     
+            onChange: function (value) {     
+                if (value === 'add_custom') {  
+                    Lampa.Input.edit({  
+                        title: Lampa.Lang.translate('location_redirect_custom_server'),  
+                        free: true,  
+                        nosave: true,  
+                        value: ''  
+                    }, (newServer) => {  
+                        if (newServer) {  
+                            // Зберігаємо новий сервер  
+                            let customServers = Lampa.Storage.get('location_custom_servers', []);  
+                            if (customServers.indexOf(newServer) === -1) {  
+                                customServers.push(newServer);  
+                                Lampa.Storage.set('location_custom_servers', customServers);  
+                            }  
+                              
+                            // Встановлюємо новий сервер  
+                            Lampa.Storage.set('location_server', newServer);  
+                              
+                            // Оновлюємо список значень  
+                            Lampa.SettingsApi.updateParam('location_redirect', 'location_server', {  
+                                values: getServerValues()  
+                            });  
+                              
+                            startMe();  
+                        } else {  
+                            Lampa.Controller.toggle('content');  
+                        }  
+                    });  
+                } else {  
+                    startMe();  
+                }  
+            }     
+        });     
+    }     
+        
+    if(window.appready) startMe();     
+    else {     
+        Lampa.Listener.follow('app', function(e) {     
+            if(e.type == 'ready') {     
+                startMe();     
+            }     
+        });     
+    }     
 })();
