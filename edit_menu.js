5111353eb4b43e39f0cc9f348f7c83e719d0eeef
kikoff1 <neris54@gmail.com>
2026-02-10 12:17:42 +0200

    Update edit_menu.js

--- edit_menu.js
+++ edit_menu.js
@@ -180,19 +180,20 @@
                 let sort = Lampa.Storage.get('menu_sort', [])  
                 let hide = Lampa.Storage.get('menu_hide', [])  
   
-                // Застосовуємо порядок тільки до першої секції  
-                let firstList = $('.menu .menu__list:eq(0)')  
+                let menu = $('.menu')  
+                if(!menu.length) return  
   
+                // Застосовуємо сортування тільки для першої секції  
                 if(sort.length) {  
                     sort.forEach((name) => {  
-                        let item = firstList.find('.menu__item').filter(function() {  
+                        let item = menu.find('.menu__list:eq(0) .menu__item').filter(function() {  
                             return $(this).find('.menu__text').text().trim() === name  
                         })  
-                        if(item.length) item.appendTo(firstList)  
+                        if(item.length) item.appendTo(menu.find('.menu__list:eq(0)'))  
                     })  
                 }  
   
-                // ВИПРАВЛЕННЯ: Застосовуємо приховування до ВСІХ пунктів меню з обох секцій  
+                // Застосовуємо приховування до ВСІХ пунктів меню з обох секцій  
                 // Спочатку знімаємо всі приховування  
                 $('.menu .menu__item').removeClass('hidden')  
   
@@ -238,7 +239,7 @@
   
                 // Додаємо кнопку очищення кешу  
                 let clearBtn = Lampa.Head.addIcon(  
-                    `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">  
+                    `<svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">  
                         <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" fill="currentColor"/>  
                     </svg>`,  
                     () => {  
@@ -252,7 +253,7 @@
                 // Додаємо кнопку перезавантаження, якщо увімкнено  
                 if (Lampa.Storage.get('add_reload_button', false)) {  
                     let reloadBtn = Lampa.Head.addIcon(  
-                        `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">  
+                        `<svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">  
                             <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" fill="currentColor"/>  
                         </svg>`,  
                         () => {  
@@ -325,26 +326,25 @@
                     titleKey = 'head_action_fullscreen';  
                 } else if (mainClass.includes('open--broadcast')) {  
                     titleKey = 'head_action_broadcast';  
+                } else if (mainClass === 'head__action--clear-cache') {  
+                    titleKey = 'head_action_clear_cache';  
+                } else if (mainClass === 'head__action--reload') {  
+                    titleKey = 'head_action_reload';  
                 }  
   
                 return titleKey ? Lampa.Lang.translate(titleKey) : Lampa.Lang.translate('no_name');  
             }
-            // Функція для редагування лівого меню (ВИПРАВЛЕНО: видалено стрілочки для другої секції)  
+            // Функція для редагування лівого меню  
             function editLeftMenu() {  
                 let list = $('<div class="menu-edit-list"></div>')  
                 let menu = $('.menu')  
   
-                // Обробляємо ВСІ пункти меню з обох секцій  
                 menu.find('.menu__item').each(function(){  
                     let item_orig = $(this)  
                     let item_clone = $(this).clone()  
-  
                     let text = item_clone.find('.menu__text').text().trim()  
-  
-                    // Перевіряємо, чи пункт з першої секції (можна переміщувати)  
                     let isFirstSection = item_orig.closest('.menu__list').is('.menu__list:eq(0)')  
-  
-                    // Створюємо HTML без кнопок переміщення для другої секції  
+                      
                     let moveButtons = isFirstSection ? `  
                         <div class="menu-edit-list__move move-up selector">  
                             <svg width="22" height="14" viewBox="0 0 22 14" fill="none" xmlns="http://www.w3.org/2000/svg">  
@@ -371,11 +371,9 @@
   
                     item_sort.find('.menu-edit-list__icon').append(item_clone.find('.menu__ico').html())  
   
-                    // Кнопки переміщення працюють тільки для першої секції  
                     if(isFirstSection) {  
                         item_sort.find('.move-up').on('hover:enter', ()=>{  
                             let prev = item_sort.prev()  
-                            // Шукаємо попередній елемент з першої секції  
                             while(prev.length && prev.data('isSecondSection')) {  
                                 prev = prev.prev()  
                             }  
@@ -387,7 +385,6 @@
   
                         item_sort.find('.move-down').on('hover:enter', ()=>{  
                             let next = item_sort.next()  
-                            // Шукаємо наступний елемент з першої секції  
                             while(next.length && next.data('isSecondSection')) {  
                                 next = next.next()  
                             }  
@@ -397,11 +394,9 @@
                             }  
                         })  
                     } else {  
-                        // Позначаємо елементи з другої секції  
                         item_sort.data('isSecondSection', true)  
                     }  
   
-                    // Приховування працює для ВСІХ пунктів  
                     item_sort.find('.toggle').on('hover:enter', ()=>{  
                         item_orig.toggleClass('hidden')  
                         item_sort.find('.dot').attr('opacity', item_orig.hasClass('hidden') ? 0 : 1)  
@@ -423,24 +418,29 @@
                 })  
             }  
   
-            // Функція для редагування верхнього меню  
+            // Початок функції для редагування верхнього меню  
             function editTopMenu() {  
                 let list = $('<div class="menu-edit-list"></div>')  
                 let head = $('.head')  
-  
+                  
+                list.empty()  
+                  
                 head.find('.head__action').each(function(){  
                     let item_orig = $(this)  
+                      
+                    if (item_orig.hasClass('head__action--clear-cache') || item_orig.hasClass('head__action--reload')) {  
+                        return  
+                    }  
+                      
                     let item_clone = $(this).clone()  
-  
                     let allClasses = item_clone.attr('class').split(' ')  
-                    let mainClass = allClasses.find(c =>  
-                        c.startsWith('open--') ||  
-                        c.startsWith('notice--') ||  
+                    let mainClass = allClasses.find(c =>   
+                        c.startsWith('open--') ||   
+                        c.startsWith('notice--') ||   
                         c.startsWith('full--')  
                     ) || ''  
-  
-                    let displayName = getHeadActionName(mainClass)  
-  
+                      
+                    let displayName = getHeadActionName(mainClass)
                     let item_sort = $(`<div class="menu-edit-list__item">  
                         <div class="menu-edit-list__icon"></div>  
                         <div class="menu-edit-list__title">${displayName}</div>  
@@ -461,12 +461,12 @@
                             </svg>  
                         </div>  
                     </div>`)  
-  
+                      
                     let svg = item_clone.find('svg')  
                     if(svg.length) {  
                         item_sort.find('.menu-edit-list__icon').append(svg.clone())  
                     }  
-  
+                      
                     item_sort.find('.move-up').on('hover:enter', ()=>{  
                         let prev = item_sort.prev()  
                         if(prev.length){  
@@ -474,7 +474,7 @@
                             item_orig.insertBefore(item_orig.prev())  
                         }  
                     })  
-  
+                      
                     item_sort.find('.move-down').on('hover:enter', ()=>{  
                         let next = item_sort.next()  
                         if(next.length){  
@@ -482,24 +482,34 @@
                             item_orig.insertAfter(item_orig.next())  
                         }  
                     })  
-  
+                      
                     item_sort.find('.toggle').on('hover:enter', ()=>{  
                         item_orig.toggleClass('hide')  
                         item_sort.find('.dot').attr('opacity', item_orig.hasClass('hide') ? 0 : 1)  
                     }).find('.dot').attr('opacity', item_orig.hasClass('hide') ? 0 : 1)  
-  
+                      
                     list.append(item_sort)  
                 })  
-  
-                // Додаємо кнопку очищення кешу до редактора  
+                  
+                // Додаємо кнопку очищення кешу  
                 let clearItem = $(`  
                     <div class="menu-edit-list__item">  
                         <div class="menu-edit-list__icon">  
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">  
+                            <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">  
                                 <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" fill="currentColor"/>  
                             </svg>  
                         </div>  
                         <div class="menu-edit-list__title">${Lampa.Lang.translate('head_action_clear_cache')}</div>  
+                        <div class="menu-edit-list__move move-up selector">  
+                            <svg width="22" height="14" viewBox="0 0 22 14" fill="none" xmlns="http://www.w3.org/2000/svg">  
+                                <path d="M2 12L11 3L20 12" stroke="currentColor" stroke-width="4" stroke-linecap="round"/>  
+                            </svg>  
+                        </div>  
+                        <div class="menu-edit-list__move move-down selector">  
+                            <svg width="22" height="14" viewBox="0 0 22 14" fill="none" xmlns="http://www.w3.org/2000/svg">  
+                                <path d="M2 2L11 11L20 2" stroke="currentColor" stroke-width="4" stroke-linecap="round"/>  
+                            </svg>  
+                        </div>  
                         <div class="menu-edit-list__toggle toggle selector">  
                             <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">  
                                 <rect x="1.89111" y="1.78369" width="21.793" height="21.793" rx="3.5" stroke="currentColor" stroke-width="3"/>  
@@ -508,24 +518,49 @@
                         </div>  
                     </div>  
                 `)  
-  
+                  
+                clearItem.find('.move-up').on('hover:enter', ()=>{  
+                    let prev = clearItem.prev()  
+                    if(prev.length){  
+                        clearItem.insertBefore(prev)  
+                        $('.head__action--clear-cache').insertBefore($('.head__action--clear-cache').prev())  
+                    }  
+                })  
+                  
+                clearItem.find('.move-down').on('hover:enter', ()=>{  
+                    let next = clearItem.next()  
+                    if(next.length){  
+                        clearItem.insertAfter(next)  
+                        $('.head__action--clear-cache').insertAfter($('.head__action--clear-cache').next())  
+                    }  
+                })  
+                  
                 clearItem.find('.toggle').on('hover:enter', ()=>{  
                     $('.head__action--clear-cache').toggleClass('hide')  
                     clearItem.find('.dot').attr('opacity', $('.head__action--clear-cache').hasClass('hide') ? 0 : 1)  
                 }).find('.dot').attr('opacity', $('.head__action--clear-cache').hasClass('hide') ? 0 : 1)  
-  
-                list.append(clearItem)  
-  
-                // Додаємо кнопку перезавантаження до редактора, якщо увімкнено  
+                  
+                list.append(clearItem)
+                // Додаємо кнопку перезавантаження якщо увімкнено  
                 if (Lampa.Storage.get('add_reload_button', false)) {  
                     let reloadItem = $(`  
                         <div class="menu-edit-list__item">  
                             <div class="menu-edit-list__icon">  
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">  
+                                <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">  
                                     <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" fill="currentColor"/>  
                                 </svg>  
                             </div>  
                             <div class="menu-edit-list__title">${Lampa.Lang.translate('head_action_reload')}</div>  
+                            <div class="menu-edit-list__move move-up selector">  
+                                <svg width="22" height="14" viewBox="0 0 22 14" fill="none" xmlns="http://www.w3.org/2000/svg">  
+                                    <path d="M2 12L11 3L20 12" stroke="currentColor" stroke-width="4" stroke-linecap="round"/>  
+                                </svg>  
+                            </div>  
+                            <div class="menu-edit-list__move move-down selector">  
+                                <svg width="22" height="14" viewBox="0 0 22 14" fill="none" xmlns="http://www.w3.org/2000/svg">  
+                                    <path d="M2 2L11 11L20 2" stroke="currentColor" stroke-width="4" stroke-linecap="round"/>  
+                                </svg>  
+                            </div>  
                             <div class="menu-edit-list__toggle toggle selector">  
                                 <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">  
                                     <rect x="1.89111" y="1.78369" width="21.793" height="21.793" rx="3.5" stroke="currentColor" stroke-width="3"/>  
@@ -534,15 +569,31 @@
                             </div>  
                         </div>  
                     `)  
-  
+                      
+                    reloadItem.find('.move-up').on('hover:enter', ()=>{  
+                        let prev = reloadItem.prev()  
+                        if(prev.length){  
+                            reloadItem.insertBefore(prev)  
+                            $('.head__action--reload').insertBefore($('.head__action--reload').prev())  
+                        }  
+                    })  
+                      
+                    reloadItem.find('.move-down').on('hover:enter', ()=>{  
+                        let next = reloadItem.next()  
+                        if(next.length){  
+                            reloadItem.insertAfter(next)  
+                            $('.head__action--reload').insertAfter($('.head__action--reload').next())  
+                        }  
+                    })  
+                      
                     reloadItem.find('.toggle').on('hover:enter', ()=>{  
                         $('.head__action--reload').toggleClass('hide')  
                         reloadItem.find('.dot').attr('opacity', $('.head__action--reload').hasClass('hide') ? 0 : 1)  
                     }).find('.dot').attr('opacity', $('.head__action--reload').hasClass('hide') ? 0 : 1)  
-  
+                      
                     list.append(reloadItem)  
                 }  
-  
+                  
                 Lampa.Modal.open({  
                     title: Lampa.Lang.translate('menu_editor_top'),  
                     html: list,  
